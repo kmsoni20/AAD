fc9e3fc2e8c040057ab7fd764f764978b1023d5e
Kartik Soni <68638578+kmsoni20@users.noreply.github.com>
2022-03-30 17:26:44 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,21 @@
+import pyttsx3
+import datetime
+
+engine = pyttsx3.init()
+voice = engine.getProperty('voices')
+engine.setProperty('voice', voice[1].id)
+newVoiceRate = 150 
+engine.setProperty('rate', newVoiceRate)
+engine.say("Hello Kaartik, This is Pondi")
+engine.runAndWait()
+
+engine.say("Let's get Started")
+engine.say("Do you want me to rep you up through your day")
+engine.runAndWait()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+speak("this is PONDI")
+
